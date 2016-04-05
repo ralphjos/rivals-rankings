4265231c11dc893b943762503053de68ea8eb23b
Ralph Arroyo <ralph_arroyo@gap.com>
2016-04-05 01:51:11 -0700

    gets port from environment, or 5000 by default

--- main.py
+++ main.py
@@ -11,4 +11,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    application.run()
+    port = int(os.environ.get("PORT", 5000))
+    application.run(host='0.0.0.0', port=port)
