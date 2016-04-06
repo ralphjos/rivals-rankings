946baba7db8cdb53084f7ba715224b68b76e156e
Ralph Arroyo <ralph_arroyo@gap.com>
2016-04-05 20:02:01 -0700

    adds endpoint to return current rankings and list of all players

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
+import json
 import os
-from flask import Flask, render_template
+from flask import Flask, Response, render_template
 
 application = Flask(__name__,
                     template_folder='frontend')
+
+# Determine `pwd` of this executing file
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 @application.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@application.route("/rankings")
+def rankings():
+    """ Return raw json from file. """
+    path = os.path.join(CURRENT_DIR, 'data_processing/data/current_rankings.json')
+    with open(path, 'r') as data:
+        ranking_data = json.load(data)
+    return Response(json.dumps(ranking_data),
+                    mimetype='application/json')
+
+
+@application.route("/players")
+def all_players():
+    """ Return raw json from file. """
+    path = os.path.join(CURRENT_DIR, 'data_processing/data/all_players.json')
+    with open(path, 'r') as data:
+        player_data = json.load(data)
+    return Response(json.dumps(player_data),
+                    mimetype='application/json')
 
 
 if __name__ == "__main__":
